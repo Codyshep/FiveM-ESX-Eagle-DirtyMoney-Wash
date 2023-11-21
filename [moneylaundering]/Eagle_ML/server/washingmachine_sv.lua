e9bb33d71379d729cddccb31c2bc5e893144811a
QUE <70960162+RTX-ROG-QUE@users.noreply.github.com>
2023-11-21 18:05:06 +0100

    Update washingmachine_sv.lua

--- [moneylaundering]/Eagle_ML/server/washingmachine_sv.lua
+++ [moneylaundering]/Eagle_ML/server/washingmachine_sv.lua
@@ -2,16 +2,24 @@ ESX = exports['es_extended']:getSharedObject()
 
 require('config')
 
-RegisterServerEvent('eaglecore:giveitem')
-AddEventHandler('eaglecore:giveitem', function(item, count)
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.addInventoryItem(item, count)
-end)
+RegisterServerEvent('eaglecore:cleanmoney')
+AddEventHandler('eaglecore:cleanmoney', function(count)
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
 
-RegisterServerEvent('eaglecore:removeitem')
-AddEventHandler('eaglecore:removeitem', function(item, count)
-	local source = source
-	local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.removeInventoryItem(item, count)
+    local found = false
+    for _, v in pairs(config.washinglocations) do
+        local dist = #(xPlayer.getCoords(true) - v)
+        if dist <= 1.5 then
+            found = true
+            break
+        end
+    end
+
+    if count >= xPlayer.getInventoryItem(config.moneytypes.dirtymoney).count or not found then
+        DropPlayer(source, 'Trying to exploit the Moneywash')
+        return
+    end
+    xPlayer.addInventoryItem(config.moneytypes.cleanmoney, count / 1.2)
+    xPlayer.removeInventoryItem(config.moneytypes.dirtymoney, count)
 end)
